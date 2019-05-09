2ce5eee7c0ebc3793a740b5e296a7162ded56bbb
ravirj21 <ravirj21@gmail.com>
2019-05-09 22:46:27 +0530

    first commit

---
+++ src/com/Bosch/Bean/DemoClass.java
@@ -0,0 +1,14 @@
+package com.Bosch.Bean;
+
+public class DemoClass {
+
+	public DemoClass() {
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Project completed successfully");
+
+	}
+
+}
